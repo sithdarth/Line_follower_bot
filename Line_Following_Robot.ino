cbaeff6227ccd30bd8b489fbe33acc9879e74a9a
ATechnoHazard <amolele@gmail.com>
2018-02-20 23:25:56 -0800

    Add the GNU Public License
    Signed-off-by: sithdarth <sith.darth.chooseaside@gmail.com>

--- Line_Following_Robot.ino
+++ Line_Following_Robot.ino
@@ -1,3 +1,11 @@
+                    GNU GENERAL PUBLIC LICENSE
+                       Version 3, 29 June 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+
+
 int in_1 = 5;
 int in_2 = 6;
 int in_3 = 10;
